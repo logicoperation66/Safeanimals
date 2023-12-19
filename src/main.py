e7fb784df24ce029f55cc997ebcc3b9f8db2f344
Adam Wawrzyniak <adamwawrzyniak5@gmail.com>
2023-12-19 21:09:23 +0100

    Fix for age values

--- src/main.py
+++ src/main.py
@@ -10,20 +10,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 login = os.getenv("sa_login")
 password = os.getenv("sa_password")
 print(login, password)
 url = 'https://www.safe-animal.eu/pl/dodaj-zwierze/Animals/add'
+months_list = ["miesięcy", "miesiecy", "miesiac", "miesiąc"]
+week_list = ["tygodni", "tydzień", "tygodnie"]
 
-if __name__ == "__main__":
-    #Open csv with bunch of data to upload
+def main():
+    # Open csv with bunch of data to upload
     with open("csv_files/BAZA DANYCH SCHRONISKO - BAZA DANYCH.csv", 'r', encoding="UTF-8") as data:
         reader = csv.reader(data)
-        SS = f"{datetime.datetime.now()}"
-        repired_SS = SS.replace(".", "-")
+        # SS = f"{datetime.datetime.now()}"
+        # repired_SS = SS.replace(".", "-")
         for i in reader:
             driver = webdriver.Chrome()
-            #Ope web browser and specific url
+            # Ope web browser and specific url
             webactions.site_opener(url=url,
                                    driver=driver)
             time.sleep(2)
@@ -31,31 +35,32 @@ if __name__ == "__main__":
                              password=password,
                              driver=driver)
             time.sleep(1)
-            #Chip
+            # Chip
             webactions.form_filler(xpath='//*[@id="zNumerId"]',
                                    variable_to_fill=i[1],
                                    driver=driver)
-            #Imie
+            # Imie
             webactions.form_filler(xpath='//*[@id="zImie"]',
                                    variable_to_fill=i[0],
                                    driver=driver)
-            #RASA
+            # RASA
             webactions.form_filler(xpath='//*[@id="zRasa"]',
                                    variable_to_fill="MIX",
                                    driver=driver)
-            #MAŚĆ
+            # MAŚĆ
             webactions.form_filler(xpath='//*[@id="zMasc"]',
                                    variable_to_fill=i[5],
                                    driver=driver)
-            #ŚIERŚĆ
+            # ŚIERŚĆ
             webactions.form_filler(xpath='//*[@id="zSiersc"]',
                                    variable_to_fill="ŚREDNI",
                                    driver=driver)
-            #Gatunek
+            # Gatunek
             webactions.select_filler_by_index(xpath='//*[@id="zGatunek"]',
                                               select_option=1,
                                               driver=driver)
-            #SEX !!! [I'm just too old for that kind of jokes...]
+
+            # SEX !!! (I think that i'm to old for that kind of jokes....)
             if i[3] == 'SAMIEC':
                 sex = 1
             else:
@@ -64,8 +69,18 @@ if __name__ == "__main__":
             webactions.select_filler_by_index(xpath='//*[@id="zPlec"]',
                                               select_option=sex,
                                               driver=driver)
-            #Birth date
-            num_of_months = i[4]
+
+            # Birth date
+            age = i[4]
+            if any(months in age for months in months_list):
+                months = [int(i) for i in age.split() if i.isdigit()]
+                num_of_months = months[0]
+            if any(weeks in age for weeks in week_list):
+                weeks = [int(i) for i in age.split() if i.isdigit()]
+                num_of_months = weeks[0] // 4  # just a +/- value. it's is'int important
+            else:
+                animal_age = [int(i) for i in age.split() if i.isdigit()]
+                num_of_months = animal_age[0]
             month, year = data_func.subtract_months_from_now(int(num_of_months))
             webactions.select_filler_by_index(xpath='//*[@id="zDataUr1"]',
                                               select_option=month,
@@ -76,14 +91,15 @@ if __name__ == "__main__":
                                              driver=driver)
             time.sleep(1)
             webactions.scroll(driver=driver)
-            ss = pyautogui.screenshot()
+            # ss = pyautogui.screenshot()
 
             time.sleep(10)
             try:
                 webactions.click(xpath='/html/body/div[2]/div[2]/section[1]/section[2]/div/div[1]/div[3]/form/ul/li[2]',
                                  driver=driver)
-            except Exception as e: print(e
-                                         )
+            except Exception as e:
+                print(e
+                      )
             time.sleep(1)
             webactions.select_filler_by_index(xpath='//*[@id="ownerType"]',
                                               select_option=1,
@@ -99,6 +115,11 @@ if __name__ == "__main__":
 
             # print("If all is ok now we should only save this shit\n and pop out this element from csv")
             print(f"I think we are done for this dog: {i[0]}, {i[1]}")
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
